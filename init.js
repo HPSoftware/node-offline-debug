8ab8ab043eb286d21d005f546e0e30ac535a6d05
Inbar Shani <inbar.shani.il@gmail.com>
2014-08-18 12:09:02 +0300

    fixed debug statemetn 'return' in postBackLog;few code cleanups

--- init.js
+++ init.js
@@ -1,16 +1,15 @@
 var config = require('./lib/config'),
   debugOffline = require('./offline_debug'),
+  logger = require('./lib/logger'),
   _instruments = require('./lib/instruments.js');
 
 // initialization code, run once when module is loaded
-var waiting = true;
 
 debugOffline(); // override module load
 
 var waitForConfigReload = setInterval(function () {
     if (config.status === "done") {
-      waiting = false;
-      console.log('Done!');
+      logger.info('Instrumentation configuration is loaded');
       config.initIntervaling();
       clearInterval(waitForConfigReload);
     }
